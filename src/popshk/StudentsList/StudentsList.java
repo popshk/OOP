03ec37016e1aa83055bb8220d96d58a65fa98945
papunilo <anonympopkov@gmail.com>
2018-06-21 17:44:46 +0300

    add Add&Delete

--- src/popshk/StudentsList/StudentsList.java
+++ src/popshk/StudentsList/StudentsList.java
@@ -1,7 +1,24 @@
 package popshk.StudentsList;
 
 class StudentsList {
-    private Student [] lisl = new Student[3];
+    private Student [] lisl = new Student[2];
+    private int i=0;
 
+    public void add(Student student){
+        if (i+1==lisl.length){
+            Student [] s = new Student[lisl.length*2];
+                System.arraycopy(lisl,0,s,0,lisl.length);
+                    lisl=s;
+        }
+            lisl[i++]=student;
+    }
+
+    public void delete(int n){
+        if(n<=0 || n>lisl.length) return;
+            Student [] s =new Student[lisl.length-1];
+                System.arraycopy(lisl,0,s,0,n);
+                System.arraycopy(lisl,n+1,s,n,s.length-n);
+                    lisl=s;
+    }
 
 }
