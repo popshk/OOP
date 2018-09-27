ffdbb5a4992b35d4c26ea0df6dd6a02a06d1c9fc
papunilo <anonympopkov@gmail.com>
2018-09-27 17:54:56 +0300

    creating....(1)

--- src/popshk/FindAndCopyFiles/CopyFiles.java
+++ src/popshk/FindAndCopyFiles/CopyFiles.java
@@ -1,11 +1,34 @@
 package popshk.FindAndCopyFiles;
 
+import java.io.*;
+
 public class CopyFiles {
 
-    String src ="/home/popshk/Рабочий стол/test1/";
-    String dest="/home/popshk/Рабочий стол/test1/testDir";
+    String srcDir ="/home/popshk/Рабочий стол/test1/";
+    String destDir="/home/popshk/Рабочий стол/test1/testDir";
 
-    public static void copy(String src,String dest){
+    public static void copy(String src,String dest) throws IOException {
+      FileInputStream fis =new FileInputStream(new File(src));
+        try {
+            FileOutputStream fos =new FileOutputStream(new File(dest));
+                try {
+                    byte [] buff=new byte[4096];
+                    int lenght;
+                    while ((lenght=fis.read(buff))>0){
+                        fos.write(buff,0,lenght);
+                    }
+                }finally {
+                    fos.close();
+                }
+        } finally {
+            fis.close();
+        }
+    }
+
+    public static void copyDir (String srcDir,String destDir){
+        File src = new File(srcDir);
+        File dest = new File(destDir);
+
 
     }
 
